f528945c95c0e6e2bc2ceeaec3bf54f4249aa63b
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-10-02 21:42:13 -0500

    chore: Add Icons image

---
+++ lib/app/imgs/imgs.dart
@@ -0,0 +1,8 @@
+class Imgs {
+  static const String iconHeart = 'assets/img/heart.png';
+  static const String iconDental = 'assets/img/dental.png';
+  static const String iconPhysiotherapy = 'assets/img/physiotherapy.png';
+  static const String iconElectrocardiogram =
+      'assets/img/electrocardiogram.png';
+  static const String iconSettings = 'assets/img/settings.png';
+}
