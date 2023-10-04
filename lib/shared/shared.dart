b44bcb9876e979bfb76b062c46cbb266c7261988
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-10-04 03:17:55 -0500

    feat: impl visor 3D server

--- lib/shared/shared.dart
+++ lib/shared/shared.dart
@@ -1,3 +1,4 @@
+export 'visor_3d/visor_3d_server.dart';
 export 'widgets/circular_item.dart';
 export 'widgets/listview_radius.dart';
 export 'widgets/menu.dart';
---
+++ lib/shared/visor_3d/visor_3d_server.dart
@@ -0,0 +1,77 @@
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
+
+import 'package:flutter/material.dart';
+import 'package:flutterconflatam/shared/visor_3d/shims/dart_ui.dart' as ui;
+
+class Visor3DServer extends StatefulWidget {
+  const Visor3DServer({
+    required this.url,
+    super.key,
+  });
+
+  final String url;
+
+  @override
+  State<Visor3DServer> createState() => _Visor3DServerState();
+}
+
+class _Visor3DServerState extends State<Visor3DServer> {
+  final globalKey = GlobalKey();
+
+  @override
+  void initState() {
+    createVideoElement();
+    super.initState();
+  }
+
+  html.DivElement _div() {
+    final div = html.DivElement();
+    div.style.height = '55px';
+    div.style.width = '100%';
+    div.style.position = 'absolute';
+    div.style.zIndex = '1';
+    div.style.background = 'black';
+    return div;
+  }
+
+  void createVideoElement() {
+    final divContent = html.DivElement();
+    divContent.style.height = '100%';
+    divContent.style.width = '100%';
+    divContent.style.position = 'relative';
+
+    final iframe = html.IFrameElement()
+      ..src = widget.url
+      ..id = 'myWidget'
+      ..style.border = 'none'
+      ..style.background = 'black'
+      ..style.margin = '0px'
+      ..style.padding = '0px'
+      ..style.width = '100%'
+      ..style.height = '100%';
+
+    final div1 = _div();
+    div1.style.top = '0px';
+
+    final div2 = _div();
+    div2.style.bottom = '0px';
+
+    divContent
+      ..append(iframe)
+      ..append(div1)
+      ..append(div2);
+
+    ui.platformViewRegistry.registerViewFactory(
+      'visorView3DModel',
+      (_) => divContent,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const RepaintBoundary(
+      child: HtmlElementView(viewType: 'visorView3DModel'),
+    );
+  }
+}
