f1afb0f5033f9c8ac8efb328d5077cdbd34b275a
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-10-04 13:45:44 -0500

    chore: Impl struct

--- lib/features/dental/dental_page.dart
+++ lib/features/dental/dental_page.dart
@@ -1,96 +1,22 @@
 // ignore_for_file: cascade_invocations, unnecessary_lambdas, avoid_print
 
 import 'package:flutter/material.dart';
-import 'package:flutterconflatam/app/env.dart';
-import 'package:flutterconflatam/features/dental/widgets/menu_dental.dart';
-import 'package:flutterconflatam/shared/shared.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterconflatam/core/human_sdk/human_interface.dart';
+import 'package:flutterconflatam/features/dental/bloc/dental_bloc.dart';
+import 'package:flutterconflatam/features/dental/screen/dental_screen.dart';
 
 class DentalPage extends StatelessWidget {
   const DentalPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.of(context).size.width * 0.15,
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const MenuHeader(),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: ColoredBox(
-                      color: Colors.grey,
-                      child: Column(
-                        children: [
-                          const Text('Periodontogra'),
-                          MenuDental(onTap: (value) {}),
-                          Expanded(
-                            child: Visor3DServer(
-                              apiKey: Env.instance.apiKey,
-                              url: Env.instance.serverView3dDentalURL,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  const ItemsTemp(),
-                  const Details(),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Details extends StatelessWidget {
-  const Details({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 470,
-      color: Colors.yellow,
-    );
-  }
-}
-
-class ItemsTemp extends StatelessWidget {
-  const ItemsTemp({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: 260,
-      color: Colors.blue,
-      height: double.infinity,
-      child: ListViewRadius(
-        itemCount: 20,
-        colorItem: Colors.grey,
-        itemBuilder: (context, index) {
-          return Item(
-            child: Center(
-              child: Text(index.toString()),
-            ),
-            childPallette:
-                (index == 0 || index == 3 || index == 2 || index == 10)
-                    ? const Center(child: Icon(Icons.add))
-                    : null,
-          );
-        },
-      ),
+    return BlocProvider(
+      lazy: false,
+      create: (context) => DentalBloc(
+        humanSDK: context.read<HumanSDK>(),
+      )..add(LoadEv()),
+      child: const DentalScreen(),
     );
   }
 }
