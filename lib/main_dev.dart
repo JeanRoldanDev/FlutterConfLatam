9442a79299752855a7b10f7a7afd9623cecb114e
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-10-04 13:47:38 -0500

    chore: Add bloc

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutterconflatam/app/app.dart';
+import 'package:flutterconflatam/core/human_sdk/human_interface.dart';
+import 'package:flutterconflatam/features/dental/dental_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,16 +13,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Conf 2023',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Scaffold(
-        body: Center(
-          child: Text('Development Mode'),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<HumanSDK>(
+          create: (context) => HumanSDK(),
         ),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Conf 2023',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          fontFamily: Fonts.poppins,
+          useMaterial3: true,
+        ),
+        home: const DentalPage(),
       ),
     );
   }
