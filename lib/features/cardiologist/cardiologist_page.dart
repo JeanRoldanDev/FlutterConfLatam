42755e91662945363fc2caee41811cdb9bfec482
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-10-04 01:54:03 -0500

    chore: Feature arq

---
+++ lib/features/cardiologist/cardiologist_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CardiologistPage extends StatelessWidget {
+  const CardiologistPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Cardiologist Page'),
+      ),
+    );
+  }
+}
