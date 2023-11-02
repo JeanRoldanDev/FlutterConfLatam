5325210d89502f702cbfdd7e9e384a3f5be69b2d
JeanRoldanDev <jeancarlos_2020@hotmail.com>
2023-11-02 00:16:33 -0500

    chore: solver warnings

--- lib/shared/visor_3d/visor_3d_server.dart
+++ lib/shared/visor_3d/visor_3d_server.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutterconflatam/app/style/ccolors.dart';
-import 'package:flutterconflatam/core/human_sdk/human_ui.dart';
+import 'package:flutterconflatam/core/core.dart';
 import 'package:flutterconflatam/shared/visor_3d/shims/dart_ui.dart' as ui;
 
 class Visor3DServer extends StatefulWidget {
@@ -30,7 +30,7 @@ class _Visor3DServerState extends State<Visor3DServer> {
     HumanUI.setToken(widget.apiKey);
     final divElement = HumanUI.createView(widget.id);
     ui.platformViewRegistry.registerViewFactory(
-      'visorView3DModel',
+      'visorView3DModel${widget.key}',
       (_) => divElement,
     );
   }
@@ -45,7 +45,7 @@ class _Visor3DServerState extends State<Visor3DServer> {
             width: 2,
           ),
         ),
-        child: const HtmlElementView(viewType: 'visorView3DModel'),
+        child: HtmlElementView(viewType: 'visorView3DModel${widget.key}'),
       ),
     );
   }
